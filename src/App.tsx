1b41a44e2eb87c17e231aa91176ca1c81c2b5701
V-Dat <vandat.k21@gmail.com>
2022-09-03 02:12:48 +0700

    #14 Create Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Header from "./Components/Layout/Header/Header";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <div className="app-root">
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <div className="home-root"></div>;
+}
+
+export default Home;
